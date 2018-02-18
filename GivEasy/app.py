72f2b833bf5e14d8dfda585558632d4a0143db0c
RYPTAR <ray.dej21@gmail.com>
2018-02-18 05:50:14 -0500

    added mongodb

--- GivEasy/app.py
+++ GivEasy/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask.ext.mongoengine import MongoEngine
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -8,8 +9,21 @@ bootstrap = Bootstrap(app)
 @app.route('/')
 
 def index():
-    return render_template('index1.html')
+    return render_template('index.html')
 
+app.config['MONGODB_SETTINGS'] = { 'db' : 'data' }
+db = MongoEngine(app)
+
+
+class location(db.Document):
+  
+  address = db.StringField(required=True)
+  coord = db.StringField(required=True)
+  lat = db.StringField(required=True)
+  long = db.StringField(required=True)
+  name = db.StringField(required=True)
+  industry = db.StringField(required=True)
+  #poster = db.ReferenceField(User)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
