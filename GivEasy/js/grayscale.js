e5c53ac049d3ee76bc8749cd2425278644974232
Neta Singer <netasinger@dyn-160-39-240-147.dyn.columbia.edu>
2018-02-17 19:02:13 -0500

    I am commenting

--- GivEasy/js/grayscale.js
+++ GivEasy/js/grayscale.js
@@ -16,6 +16,7 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
+  // whatsup?? 
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
   });
